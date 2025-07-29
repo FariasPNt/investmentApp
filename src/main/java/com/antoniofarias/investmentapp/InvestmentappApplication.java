cb1455259431cc79e8b1dc52f453e92a8edc106b
Antonio Farias <farias_neto@outlook.com>
2025-07-29 00:13:20 -0300

    first

---
+++ src/main/java/com/antoniofarias/investmentapp/InvestmentappApplication.java
@@ -0,0 +1,13 @@
+package com.antoniofarias.investmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniofarias/investmentapp/InvestmentappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniofarias.investmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
